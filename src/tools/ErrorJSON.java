9e4005ff8656d31cfeda11a44e4c9f7085754c35
AKNOUCHEanis <ga_aknouche@esi.dz>
2020-02-04 18:42:49 +0100

    first commit

---
+++ src/tools/ErrorJSON.java
@@ -0,0 +1,17 @@
+package tools;
+
+import org.json.simple.JSONObject;
+
+public class ErrorJSON {
+	
+	public static JSONObject serviceRefused(String message, int codeErreur)
+	{
+		
+	}
+	
+	public static JSONObject serviceAccepted()
+	{
+		
+	}
+
+}
