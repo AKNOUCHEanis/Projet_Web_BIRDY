0eac10d87c05ef9d4382ff8555071ca83465248c
3801659 <3801659@ppti-14-305-13.ufr-info-p6.jussieu.fr>
2020-02-26 15:49:58 +0100

    Fin sql

--- src/bd/BdStatic.java
+++ src/bd/BdStatic.java
@@ -7,11 +7,11 @@ import org.json.JSONObject;
 public class BdStatic {
 
 	//info statique sur la bd
-	public final static boolean mysql_pooling=true;
-	public final static String  mysql_host="";
-	public final static String  mysql_db="";
-	public final static String  mysql_username="";
-	public final static String  mysql_password="";
+	public final static boolean mysql_pooling=false;
+	public final static String  mysql_host="localhost";
+	public final static String  mysql_db="hacene_anis";
+	public final static String  mysql_username="root";
+	public final static String  mysql_password="root";
 	public final static String  mongo_host="mongodb://localhost:27017";
 	public final static String  mongo_db="DataBaseMongo_Anis_Hacene";
 	
